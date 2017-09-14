71c1bf3c0c8e374547ad05cbbf617a07291fda1a
Yuhui Huang <mrhuangyuhui@gmail.com>
2017-09-14 13:28:25 +0000

    + Save

--- mblog/mblog/settings.py
+++ mblog/mblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_f^!aqy=(#%+k1p-o6xy6hy^c4mm%itg(cn6$jt0!7#!mg_r_7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['128.199.153.136',]
+ALLOWED_HOSTS = ['128.199.197.220',]
 
 
 # Application definition
