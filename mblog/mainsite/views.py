f76ab9438e0c33b96e9164eececf329028785d5e
mrhuangyuhui <mrhuangyuhui@gmail.com>
2017-09-15 21:22:40 +0800

    + 增加详情页

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -1,5 +1,6 @@
 from django.template.loader import get_template
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from datetime import datetime
 from .models import Post
 
@@ -9,3 +10,13 @@ def homepage(request):
     now = datetime.now()
     html = template.render(locals())
     return HttpResponse(html)
+
+def showpost(request, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
